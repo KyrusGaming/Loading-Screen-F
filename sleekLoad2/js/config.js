599fa5a847f94c88eeff2b5cd0d9d5d924992d24
KyrusGaming <jwarrocks@yahoo.com>
2017-12-29 18:47:08 -0500

    nb

--- sleekLoad2/js/config.js
+++ sleekLoad2/js/config.js
@@ -53,8 +53,7 @@ Place one or more images
 Multiple images will automatically shuffle between them
 (Put the files in the backgrounds/images folder)*/
 var l_bgImages = [
-	"vader.jpg",
-	"walker.jpg",
+	"clone.jpg",
 	"moon.jpg"
 ];
 
